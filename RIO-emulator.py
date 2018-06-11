b31b89d0254d337b37341a409ef200d5bb5a2a9d
Evan Pratten <ewpratten@retrylife.ca>
2018-06-11 18:03:54 -0400

    [gitbot] autocommit

--- RIO-emulator.py
+++ RIO-emulator.py
@@ -1,3 +1,10 @@
+import cv2
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+from threading import Thread
+import imutils
+import sys
+# import StringIO
 import time
 from networktables import NetworkTables
 import socket
@@ -9,11 +16,13 @@ logging.basicConfig(level=logging.DEBUG)
 
 NetworkTables.initialize()
 
+capture=None
+
 def valueChanged(table, key, value, isNew):
-    print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
+	print("valueChanged: key: '%s'; value: %s; isNew: %s" % (key, value, isNew))
 
 def connectionListener(connected, info):
-    print(info, '; Connected=%s' % connected)
+	print(info, '; Connected=%s' % connected)
 
 
 NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
@@ -21,5 +30,111 @@ NetworkTables.addConnectionListener(connectionListener, immediateNotify=True)
 sd = NetworkTables.getTable("SmartDashboard")
 sd.addEntryListener(valueChanged)
 
-while True:
-    time.sleep(1)
+# while True:
+#     time.sleep(1)
+
+	
+	
+
+
+class CamHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        print(self.path)
+        if self.path.endswith('/stream.mjpg'):
+            self.send_response(20)
+            self.send_header('Content-type', 'multipart/x-mixed-replace; boundary=--jpgboundary')
+            self.end_headers()
+            while True:
+                try:
+
+                    
+                    r, buf = cv2.imencode(".jpg", frame)
+                    self.wfile.write("--jpgboundary\r\n".encode())
+                    self.end_headers()
+                    self.wfile.write(bytearray(buf))
+                except KeyboardInterrupt:
+                    break
+            return
+
+        if self.path.endswith('.html') or self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write('<html><head></head><body>')
+            self.wfile.write('<img src="http://localhost:1181/stream.mjpg" height="240px" width="320px"/>')
+            self.wfile.write('</body></html>')
+            return
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+
+class WebcamVideoStream:
+    def __init__(self, src=0):
+        # initialize the video camera stream and read the first frame
+        # from the stream
+        self.stream = cv2.VideoCapture(src)
+        # self.stream.set(3, 1920)
+        # self.stream.set(4, 1080)
+        # self.stream.set(15,-100)
+        (self.grabbed, self.frame) = self.stream.read()
+
+        # initialize the variable used to indicate if the thread should
+        # be stopped
+        self.stopped = False
+
+    def start(self):
+        # start the thread to read frames from the video stream
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        # keep looping infinitely until the thread is stopped
+        while True:
+            # if the thread indicator variable is set, stop the thread
+            if self.stopped:
+                self.stream.release()
+                return
+
+            # otherwise, read the next frame from the stream
+            (self.grabbed, self.frame) = self.stream.read()
+
+    def read(self):
+        # return the frame most recently read
+        return self.frame
+
+    def stop(self):
+        # indicate that the thread should be stopped
+        self.stopped = True
+
+
+def realmain():
+    global frame
+
+    ip = '0.0.0.0'
+
+    try:
+        cap = WebcamVideoStream().start()
+        server = ThreadedHTTPServer((ip, 1181), CamHandler)
+        print("starting server")
+        target = Thread(target=server.serve_forever,args=())
+
+        i = 0
+        while True:
+
+            # img = cap.read()
+            # img1 = imutils.resize(img, width=600)
+            # img2 = cv2.GaussianBlur(img1, (5, 5), 0)
+            #frame = cv2.cvtColor(img2, cv2.COLOR_BGR2HSV)
+
+            frame = cap.read()
+            # cv2.Canny(img, 35, 125)
+            if(i == 0):
+                target.start()
+            i +=1
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+if __name__ == '__main__':
+    realmain()
--- RPI.py
+++ RPI.py
@@ -1,114 +1,144 @@
-# thanks to: https://www.bluetin.io/opencv/opencv-color-detection-filtering-python/
+######## Start Notes ########
+# To install all libraries, run:
+# pip install scipy numpy imutils opencv-python-headless pynetworktables
 
-# import the necessary packages
+# The URL for Mjpg streams off the RoboRIO is:
+# http://10.TE.AM.2:1181/stream.mjpg
+######## End Notes ########
+
+# Import Required Libraries
 from collections import deque
 from imutils.video import VideoStream
 import argparse
 import cv2
 import imutils
 import time
+import urllib.request
 from scipy.interpolate import interp1d
 from networktables import NetworkTables
 import logging
-logging.basicConfig(level=logging.DEBUG)
 
-cameraid = 1
 
-accu = 10 #how much of a buffer on detect?
-deadzone = 10 #deadzone on eather side of center (in pixels)
-ranget = 0.8 #range on speed
-rangeb = -0.8 #range on speed
-# size 0-600
-
-m = interp1d([0, 600], [rangeb, ranget])
-r = range((300 - deadzone +1), (300 + deadzone +1))
-
-ip = "10.50.24.2"
-
-NetworkTables.initialize(server=ip)
-
-NetworkTables.initialize()
-sd = NetworkTables.getTable("SmartDashboard")
-
-def sendc(x):
-    sd.putNumber('camx', (x * 100 ))
-
-def calcSpeed(x, notav):
-    if(notav == 0):
-
-        speed = float("{0:.2f}".format(float(m(x))))
-        # print(speed)
-        # print(x)
-        return speed
-    else:
-        return 0.0
-
-def prepSend(x):
-    if x in r:
-        print("0.0")
-        return 0.0
-    else:
-        print(x)
-        return x
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-b", "--buffer", type=int, default=64,
-                help="max buffer size")
-args = vars(ap.parse_args())
+######## Start Configuration ########
+is_logging =  True         # Setting to False will speed up program
+ip_address = "10.50.24.2"  # Default IP address of RoboRIO
+camera_id  =  0            # Default camera id
+speed      =  0.535        # How fast should the robot move forward? (ranges from -1.0 to +1.0)
+accuracy   =  10           # How confident should the robot be before locking on to an object? (ranges from 0 to camera height)
+deadzone   =  5            # Deadzone around center of camera in pixels
+max_range  =  0.8          # Max speed
+min_range  = -0.8          # Min speed
+######## End Configuration ########
 
 # HSL bounds of "Green" (actually yellow)
-greenLower = (29, 86, 6)
-greenUpper = (64, 255, 255)
+greenLower = (29, 86, 6)    # Lower bound
+greenUpper = (64, 255, 255) # Upper bound
+
+# Set arguments and create help info
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--ip",     type=str, default=ip_address, help="Ip address of networktables server")
+ap.add_argument("-b", "--buffer", type=int, default=64,         help="max buffer size")
+ap.add_argument("-c", "--camera", type=int, default=camera_id,  help="Camera Port")
+args = vars(ap.parse_args())
+
+# Create the Mjpg server URL from data above
+server  = "http://"
+server += args["ip"]
+server += "/stream.mjpg"
+
+# Enable logging to stdout & Initialize NetworkTables
+logging.basicConfig(level=logging.DEBUG)      # Set logging mode
+NetworkTables.initialize(server=args["ip"])   # Init NT with RoboRIO IP address
+NetworkTables.initialize()                    # Finish NT init
+sd = NetworkTables.getTable("SmartDashboard") # Get SmartDashboard table
+
+# Set max buffer size based on argument
 pts = deque(maxlen=args["buffer"])
 
+# If NOT using Mjpg stream, Init camera
+if(args["camera"] != 99):
+    vs = VideoStream(src=args["camera"]).start() # Init camera
 
-vs = VideoStream(src=cameraid).start()
+# Create Maps
+pixel_map    = interp1d([0, 600], [min_range, max_range])      # Map pixel value to motor speed
+deadzone_map = range((300 - deadzone +1), (300 + deadzone +1)) # Handle deadzones
 
-# allow the camera to warm up
-time.sleep(2.0)
+######## Start Functions ########
+def LOG(x):
+	if is_logging:
+		print(x)
 
-while True:
-    # grab the current frame
-    frame = vs.read()
+def convert(frame):
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0) # blur
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert colors
+    return hsv
+
+def sendData(x,y):
+    if(y == 0 or y == 2):
+        s = float("{0:.2f}".format(float(pixel_map(x))))
+    else:
+        s = 0.0
+    if s in deadzone_map:
+        z = 0.0
+    else:
+        z = s
+    sd.putNumber('camx', (z * 100 ))
+    sd.putNumber('fspeed', (speed * 100))
+    LOG(z)
+
+######## End Functions ########
+
+
+time.sleep(2.0) # Pause for camera to start up
+while True: # Main loop
+    
+    if(args["camera"] == 99):
+        with urllib.request.urlopen(server) as url:
+            frame = url.read()
+        frame = np.array(bytearray(frame), dtype=np.uint8)
+        frame = cv2.imdecode(frame, -1)
+
+    else:
+        frame = vs.read()
 
     # handle the frame from VideoCapture or VideoStream
     frame = frame[1] if args.get("video", False) else frame
 
-    # if we are viewing a video and we did not grab a frame,
-    # then we have reached the end of the video
+    # If frame not found, the video has stopped
     if frame is None:
         break
+    
+    # Resize frame
+    frame = imutils.resize(frame, width=600)
+    
+    # Get HSV varsion of frame
+    hsv = convert(frame)
 
-    frame = imutils.resize(frame, width=600)  #resize
-    blurred = cv2.GaussianBlur(frame, (11, 11), 0) # blur
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV) # convert colors
-
-    # build mask
+    # Build frame mask
     mask = cv2.inRange(hsv, greenLower, greenUpper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
-    # find contours in the mask and initialize the current (x, y) center
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
+    # Find contours in the mask and initialize the current (x, y) center
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     center = None
 
-    # only proceed if at least one contour was found
+    # Only proceed if at least one contour was found
     if len(cnts) > 0:
+    	# Find center of cube and radius
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-
-        if radius > accu:
-            sendc(prepSend(calcSpeed(int(x), 0)))
+        
+        # Decide weather to send a speed or 0 depending on confidence
+        if radius > accuracy:
+            sendData(int(x), 0) # Send calculated speed
         else:
-            sendc(prepSend(calcSpeed(0, 1)))
+            sendData(0,1) # Send 0.0
 
-    # update the points queue
+    # Update the points queue
     pts.appendleft(center)
 
 if not args.get("video", False):
@@ -116,4 +146,5 @@ if not args.get("video", False):
 
 else:
     vs.release()
-cv2.destroyAllWindows()
+# On some computers, the following line is needed
+#cv2.destroyAllWindows()
